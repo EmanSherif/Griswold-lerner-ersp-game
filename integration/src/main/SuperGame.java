367ccc794e156f7e1886c3aa7f32b66ea133d1fe
emansherif <esherif@ucsd.edu>
2019-11-22 14:48:59 -0800

    SuperGame class

--- integration/src/main/SuperGame.java
+++ integration/src/main/SuperGame.java
@@ -1,4 +1,15 @@
 package main;
 
-public class SuperGame {
+public abstract class SuperGame {
+    public void printCode() {
+
+    }
+    public abstract void analyzeInput();
+    public void checkGameOver(){
+
+    }
+    public void updateScore(){
+
+    }
+    public abstract void acquireInput();
 }
