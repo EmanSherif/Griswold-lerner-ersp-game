aa8594ca062df8c763286f7a98cdcc94c51a14cd
Andy Liu <56865334+AndyLiu476@users.noreply.github.com>
2020-01-09 19:47:15 -0800

    Add files via upload

---
+++ BattleHUD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHUD : MonoBehaviour
+{
+    public Text HPText;
+    public Text AttackText;
+    public Slider HP;
+
+    public void SetHUD(Unit unit) {
+      HPText.text = unit.unithp;
+      AttackText.text = "Attack: " + unit.attack;
+      HP.maxValue = unit.maxHP;
+      HP.value = unit.currentHP;
+    }
+
+    public void SetHP(int hp) {
+      HP.value = hp;
+      HPText.text = hp + "/" + HP.maxValue;
+    }
+}
